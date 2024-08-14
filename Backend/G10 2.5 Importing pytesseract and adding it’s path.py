6dd2efe04011c62543a4b6f48b86b6641a4e5015
Avani <avani.abhijit@orchids.edu.in>
2024-08-14 09:45:39 +0530

    Add files via upload

---
+++ Backend/G10 2.5 Importing pytesseract and adding it’s path.py
@@ -0,0 +1,5 @@
+#Download the pytesseract to your system and copy the path.
+#https://sourceforge.net/projects/tesseract-ocr.mirror/
+
+import pytesseract #importing pytesseract module
+pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe' #set the path of the pytesseract file
